6afa19ce8e56e5b8df8a432e656775f20449e751
kampffrosch94 <kampffrosch94@github.com>
2023-09-26 01:15:37 +0200

    simplify

--- flake.nix
+++ flake.nix
@@ -4,26 +4,11 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-        libPath = with pkgs; lib.makeLibraryPath [
-          libxkbcommon
-          wayland
-          libGL
-          alsaLib
-        ];
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            rustc
-            clang
-            pkg-config
-            pkgconfig
-            udev
-            alsaLib
-            ldtk
-          ];
-          LD_LIBRARY_PATH = libPath;
-          buildInputs = with pkgs; [ openssl ];
+          packages = with pkgs; [ cargo rustc clang pkg-config alsaLib ldtk ];
+          LD_LIBRARY_PATH = with pkgs;
+            lib.makeLibraryPath [ libxkbcommon wayland libGL alsaLib ];
         };
       });
 }
