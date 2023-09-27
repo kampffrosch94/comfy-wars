9738e5a6885f5c3ea27e7015f1a0d82f77e9bc13
kampffrosch94 <kampffrosch94@github.com>
2023-09-27 20:05:04 +0200

    using mold with nix

--- flake.nix
+++ flake.nix
@@ -12,9 +12,13 @@
           # necessary to build
           cargo
           rustc
-          #clang
           pkg-config # locate C dependencies
           alsaLib # sound
+
+          # these two can be used to speed up the build
+          # add the following to .cargo/config.toml
+          clang
+          mold
 
           # extra tooling
           ldtk # level editor
@@ -30,6 +34,19 @@
             libGL # OpenGL I think
             alsaLib # sound
           ];
+
+        # making the linking go fast (thank you mr mold)
+        shellHook = ''
+          if [ ! -d ".cargo" ]; then
+            echo "creating .cargo/config.toml"
+            mkdir .cargo
+cat << 'EOF' > .cargo/config.toml
+[target.x86_64-unknown-linux-gnu]
+linker = "clang"
+rustflags = ["-Clink-arg=-fuse-ld=mold"]
+EOF
+          fi
+        '';
       };
     };
 }
