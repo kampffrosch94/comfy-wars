20facba97e53247c49793659b701f284e1e51bd0
kampffrosch94 <kampffrosch94@github.com>
2023-10-02 00:02:31 +0200

    switch to fenix (stable rust though)

--- flake.nix
+++ flake.nix
@@ -1,54 +1,62 @@
-# use with `nix develop`
-# this flake assumes x86_64-linux with wayland
 {
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs"; };
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = { nixpkgs, fenix, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      forAllSystems = function:
+        # insert more systems here
+        nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system:
+          function (import nixpkgs {
+            inherit system;
+            overlays = [ fenix.overlays.default ];
+          }));
+
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          # necessary to build
-          cargo
-          rustc
-          pkg-config # locate C dependencies
-          alsaLib # sound
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # rust stuff
+            (with pkgs.fenix; with stable; combine [
+              cargo
+              clippy
+              rust-src
+              rustc
+              rustfmt
+              targets.wasm32-unknown-unknown.stable.rust-std
+            ])
 
-          # these two can be used to speed up the build
-          # add the following to .cargo/config.toml
-          clang
-          mold
+            clang
+            mold
+            trunk
+            # rust-analyzer-nightly # optional
 
-          # extra tooling
-          ldtk # level editor
-          jq # extract stuff from json
-          tracy # profiler, call with ~Tracy~
-          cargo-flamegraph # more profiling :)
-          cargo-watch
-          # cargo-llvm-lines
-        ];
-        # stuff we need to run
-        LD_LIBRARY_PATH = with pkgs;
-          lib.makeLibraryPath [
+            # necessary to build
+            pkg-config # locate C dependencies
+            alsaLib # sound
             libxkbcommon # keyboard
             wayland
-            libGL # OpenGL I think
-            alsaLib # sound
-          ];
 
-        # making the linking go fast (thank you mr mold)
-        shellHook = ''
-          if [ ! -d ".cargo" ]; then
-            echo "creating .cargo/config.toml"
-            mkdir .cargo
-cat << 'EOF' > .cargo/config.toml
-[target.x86_64-unknown-linux-gnu]
-linker = "clang"
-rustflags = ["-Clink-arg=-fuse-ld=mold"]
-EOF
-          fi
-        '';
-      };
+            # extra tooling
+            ldtk # level editor
+            jq # extract stuff from json
+            tracy # profiler, call with ~Tracy~
+            cargo-flamegraph # more profiling :)
+            cargo-watch
+          ];
+          # stuff we need to run
+          LD_LIBRARY_PATH = with pkgs;
+            lib.makeLibraryPath [
+              libxkbcommon # keyboard
+              wayland
+              libGL # OpenGL I think
+              alsaLib # sound
+            ];
+        };
+      });
     };
 }
