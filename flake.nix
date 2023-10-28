2f948e46d608af7d93b378517fc5bec85b3649f3
kampffrosch94 <kampffrosch94@github.com>
2023-10-28 20:20:34 +0200

    update comfy

--- flake.nix
+++ flake.nix
@@ -1,62 +1,64 @@
+# use with `nix develop
+# this flake assumes x86_64-linux with wayland
 {
-  inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-  };
-  outputs = { nixpkgs, fenix, ... }:
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  outputs = { self, nixpkgs, ... }:
     let
-      forAllSystems = function:
-        # insert more systems here
-        nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system:
-          function (import nixpkgs {
-            inherit system;
-            overlays = [ fenix.overlays.default ];
-          }));
-
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # rust stuff
-            (with pkgs.fenix; with stable; combine [
-              cargo
-              clippy
-              rust-src
-              rustc
-              rustfmt
-              targets.wasm32-unknown-unknown.stable.rust-std
-            ])
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # working with x
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
 
-            clang
-            mold
-            trunk
-            # rust-analyzer-nightly # optional
+          # necessary to build
+          cargo
+          rustc
+          pkg-config # locate C dependencies
+          alsaLib # sound
 
-            # necessary to build
-            pkg-config # locate C dependencies
-            alsaLib # sound
+          # these two can be used to speed up the build
+          # add the following to .cargo/config.toml
+          clang
+          mold
+
+          # extra tooling
+          ldtk # level editor
+          jq # extract stuff from json
+          tracy # profiler, call with ~Tracy~
+          cargo-flamegraph # more profiling :)
+          cargo-watch
+          # cargo-llvm-lines
+        ];
+        # stuff we need to run
+        LD_LIBRARY_PATH = with pkgs;
+          lib.makeLibraryPath [
             libxkbcommon # keyboard
             wayland
-
-            # extra tooling
-            ldtk # level editor
-            jq # extract stuff from json
-            tracy # profiler, call with ~Tracy~
-            cargo-flamegraph # more profiling :)
-            cargo-watch
+            libGL # OpenGL I think
+            alsaLib # sound
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
           ];
-          # stuff we need to run
-          LD_LIBRARY_PATH = with pkgs;
-            lib.makeLibraryPath [
-              libxkbcommon # keyboard
-              wayland
-              libGL # OpenGL I think
-              alsaLib # sound
-            ];
-        };
-      });
+
+        # making the linking go fast (thank you mr mold)
+        shellHook = ''
+                    if [ ! -d ".cargo" ]; then
+                      echo "creating .cargo/config.toml"
+                      mkdir .cargo
+cat << 'EOF' > .cargo/config.toml
+[target.x86_64-unknown-linux-gnu]
+linker = "clang"
+rustflags = ["-Clink-arg=-fuse-ld=mold"]
+EOF
+                    fi
+        '';
+      };
     };
 }
